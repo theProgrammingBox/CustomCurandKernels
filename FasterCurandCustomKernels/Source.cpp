361a70e4f937eb253278dcf19a20d65e1512e4e6
theProgrammingBox <justinboxgea@gmail.com>
2023-04-24 21:55:44 -0700

    made an efficent uniform __half from -1 to 1

--- FasterCurandCustomKernels/Source.cpp
+++ FasterCurandCustomKernels/Source.cpp
@@ -7,7 +7,7 @@
 
 __global__ void cudaFill(void* output, uint64_t seed, uint64_t offset)
 {
-    //const __half scale = __float2half(1.0f / 0xffff);
+    const __half scale = __float2half(0.000030517578125f);
     const uint64_t idx = ((uint64_t)blockIdx.x << 10) + threadIdx.x;
     seed ^= (uint64_t)__brev((uint32_t)seed) << 32;
     offset += idx;
@@ -17,18 +17,26 @@ __global__ void cudaFill(void* output, uint64_t seed, uint64_t offset)
     offset ^= (offset >> 35) + 8;
     offset *= 0x9FB21C651E98DF25ULL;
     offset ^= offset >> 28;
-    //*((uint64_t*)output + idx) = offset;
-    *((__half*)output + idx) = __float2half((offset & 0xffff) * 0.0000152587890625f);
-    *((__half*)output + idx + 1) = __float2half(((offset >> 16) & 0xfff) * 0.0000152587890625f);
-    *((__half*)output + idx + 2) = __float2half(((offset >> 32) & 0xfff) * 0.0000152587890625f);
-    *((__half*)output + idx + 3) = __float2half(((offset >> 48) & 0xfff) * 0.0000152587890625f);
+    __half arr[4];
+    arr[0] = __hmul(__float2half(offset & 0x7fff), scale);
+    arr[1] = __hmul(__float2half((offset >> 16) & 0x7fff), scale);
+    arr[2] = __hmul(__float2half((offset >> 32) & 0x7fff), scale);
+    arr[3] = __hmul(__float2half((offset >> 48) & 0x7fff), scale);
+    *((uint64_t*)arr) ^= offset & 0x8000800080008000;
+    *((uint64_t*)output + idx) = *((uint64_t*)arr);
+    // __half* location = (__half*)output + (idx << 2);
+    // location[0] = __hmul(__float2half(offset & 0x7fff), scale);
+    // location[1] = __hmul(__float2half((offset >> 16) & 0x7fff), scale);
+    // location[2] = __hmul(__float2half((offset >> 32) & 0x7fff), scale);
+    // location[3] = __hmul(__float2half((offset >> 48) & 0x7fff), scale);
+    // *((uint64_t*)output + idx) ^= offset & 0x8000800080008000;
 }
 
 void Fill(void* output, uint64_t f16s, uint64_t& seed, uint64_t& offset)
 {
     // assert multiple of 4096
     assert((f16s & 0xfff) == 0);
-    cudaFill<<<f16s >> 12, 0x400>>>(output, seed, offset);
+    cudaFill << < f16s >> 12, 0x400 >> > (output, seed, offset);
     seed++;
     offset += 3;
 }
@@ -37,16 +45,16 @@ void TestStats(void* gpuArr, void* cpuArr, uint64_t f16s)
 {
     cudaMemcpy(cpuArr, gpuArr, f16s << 2, cudaMemcpyDeviceToHost);
 
-    uint64_t sum = 0;
+    double sum = 0;
     for (uint64_t i = 0; i < f16s; i++)
-        sum += ((uint16_t*)cpuArr)[i];
-    double mean = (double)sum / f16s;
+        sum += __half2float(((__half*)cpuArr)[i]);
+    double mean = sum / f16s;
     printf("mean: %f\n", mean);
 
     double variance = 0;
     for (uint64_t i = 0; i < f16s; i++)
     {
-        double diff = ((uint16_t*)cpuArr)[i] - mean;
+        double diff = __half2float(((__half*)cpuArr)[i]) - mean;
         variance += diff * diff;
     }
     variance /= f16s;
@@ -58,7 +66,8 @@ void TestStats(void* gpuArr, void* cpuArr, uint64_t f16s)
 
 int main()
 {
-    const uint64_t f16s = (uint64_t)1 << 12;
+    const uint64_t f16s = (uint64_t)1 << 24;
+    const uint16_t iterations = 0xfff;
 
     void* cpuArr = malloc(f16s << 2);
     void* gpuArr;
@@ -74,25 +83,15 @@ int main()
     curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
     curandSetPseudoRandomGeneratorSeed(gen, seed);
 
-    printf("Testing custom kernel...\n");
-    averageMilliseconds = 0;
-    for (uint8_t i = 0xff; i--;)
+    //warmup
+    for (uint16_t i = iterations; i--;)
     {
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
-        cudaEventRecord(start);
         Fill(gpuArr, f16s, seed, offset);
-        cudaEventRecord(stop);
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&milliseconds, start, stop);
-        averageMilliseconds += milliseconds;
     }
-    printf("average time: %fms\n", averageMilliseconds / 0xff);
-    TestStats(gpuArr, cpuArr, f16s);
 
     printf("Testing cuRAND...\n");
     averageMilliseconds = 0;
-    for (uint8_t i = 0xff; i--;)
+    for (uint16_t i = iterations; i--;)
     {
         cudaEventCreate(&start);
         cudaEventCreate(&stop);
@@ -103,16 +102,39 @@ int main()
         cudaEventElapsedTime(&milliseconds, start, stop);
         averageMilliseconds += milliseconds;
     }
-    printf("average time: %fms\n", averageMilliseconds / 0xff);
-    TestStats(gpuArr, cpuArr, f16s);
+    printf("average time: %fms\n", averageMilliseconds / iterations);
+    // TestStats(gpuArr, cpuArr, f16s);
+    printf("\n");
+    double curandTime = averageMilliseconds / iterations;
 
-    /*for (uint64_t i = 0; i < f16s; i++)
+    printf("Testing custom kernel...\n");
+    averageMilliseconds = 0;
+    for (uint16_t i = iterations; i--;)
     {
-        uint16_t* ptr = (uint16_t*)cpuArr + i;
-        for (uint8_t j = 16; j--;)
-            printf("%u", (bool)((*ptr >> j) & 1));
-        printf("\n");
-    }*/
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        cudaEventRecord(start);
+        Fill(gpuArr, f16s, seed, offset);
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        averageMilliseconds += milliseconds;
+    }
+    printf("average time: %fms\n", averageMilliseconds / iterations);
+    TestStats(gpuArr, cpuArr, f16s);
+    double customTime = averageMilliseconds / iterations;
+
+    printf("A %f%% difference in performance.\n\n", (curandTime - customTime) / curandTime * 100);
+
+    for (uint64_t i = 0; i < 0xf; i++)
+    {
+        // uint16_t* ptr = (uint16_t*)cpuArr + i;
+        // for (uint8_t j = 16; j--;)
+        //     printf("%u", (bool)((*ptr >> j) & 1));
+        // printf("\n");
+        printf("%f\n", __half2float(((__half*)cpuArr)[i]));
+    }
+    printf("\n");
 
     free(cpuArr);
     cudaFree(gpuArr);
